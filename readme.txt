d3001658807afd271440fcd8686a9853a905e603
twodayslate <twodayslate@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-28 22:05:03 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/recent-categories/trunk@1148476 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Plugin Name ===
+Contributors: twodayslate
+Donate link: http://zac.gorak.us
+Tags: categories
+Requires at least: 4.0
+Tested up to: 4.2
+Stable tag: 4.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Show your site's most recent categories!
+
+== Description ==
+
+Widget to display your site's most recent categories. 
+
+GitHub: https://github.com/twodayslate/Recent-Categories-Widget
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `widget.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place `<?php do_action('plugin_name_hook'); ?>` in your templates
+
+== Frequently Asked Questions ==
+
+= What options are there? =
+
+You can set the max number of categories to show, the title, and the date.
+
+== Screenshots ==
+
+1. screenshot/screenshot.png
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release
+
+== Upgrade Notice == 
+
+= 1.0 =
+First release. 
---
+++ widget.php
@@ -0,0 +1,175 @@
+<?php
+/* 
+Plugin Name: Recent Categories
+Plugin URI: http://zac.gorak.us
+Description: Recent Categories
+Author: @twodayslate
+Version: 1.0
+Author URI: http://zac.gorak.us
+*/
+
+// Block direct requests
+if ( !defined('ABSPATH') )
+	die('-1');
+	
+	
+add_action( 'widgets_init', function(){
+     register_widget( 'Recent_Categories_Widget' );
+});	
+
+/**
+ * Adds Recent_Categories_Widget widget.
+ */
+class Recent_Categories_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+
+
+        function __construct() {
+
+
+
+		parent::__construct(
+			'Recent_Categories_Widget', // Base ID
+			__('Recent Categories', 'text_domain'), // Name
+			array('description' => __( "You site's most recent categories.", 'text_domain' ),) // Args
+		);
+
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		
+		// get the excerpt of the required story
+		if ( isset( $instance[ 'max_count' ] ) ) {
+			$max_count = $instance[ 'max_count' ];
+		}
+		else {
+			$max_count = 5;
+		}
+
+		if ( isset( $instance[ 'title' ] ) && !empty($instance['title'] )) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = "Recent Categories";
+		}
+		if ( isset( $instance[ 'display_date' ] ) ) {
+			$display_date = $instance[ 'display_date' ] ? true : false;
+		}
+		
+		//if ( array_key_exists('before_widget', $args) ) echo $args['before_widget'];
+
+		if (array_key_exists('before_widget', $args)) {
+
+                echo str_replace('widget_recent_categories_widget', 'widget_recent_categories_widget widget_recent_entries', $args['before_widget']);
+        
+        }
+		
+		$args = array(
+	    'numberposts' => $max_count,
+	    'offset' => 0,
+	    'orderby' => 'post_date',
+	    'order' => 'DESC',
+	    'post_type' => 'post',
+	    'post_status' => 'publish',
+	    'suppress_filters' => true );
+
+	    $recent_posts = get_posts( $args );
+
+	    echo '<h2 class="widget-title">'.$title.'</h2>';
+	    echo "<ul>";
+	    $listed_categories = array();
+	    foreach ($recent_posts as $apost) {
+	    	$categories = get_the_category($apost->ID);
+			foreach ($categories as $acategory) {
+				if(!in_array($acategory->cat_ID, $listed_categories)){
+					echo '<li><a href="'.get_category_link($acategory->cat_ID).'">'.$acategory->cat_name.'</a>';
+					if($display_date) {
+						echo '<span class="post-date">'.get_the_date(get_option('date_format'),$apost->ID).'</span>';
+					}
+					echo "</li>";
+					array_push($listed_categories,$acategory->cat_ID);
+				}
+			}
+	    }
+	    echo "</ul>";
+			
+		if ( array_key_exists('after_widget', $args) ) {
+			echo $args['after_widget'];
+		} else {
+			echo "</aside>";
+		}
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		
+		if ( isset( $instance[ 'max_count' ] ) ) {
+			$max_count = $instance[ 'max_count' ];
+		}
+		else {
+			$max_count = 5;
+		}
+
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+
+		?>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?>
+				<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title ?>" />
+			</label> 
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'max_count' ); ?>"><?php _e( 'Max number of categories to show:' ); ?>
+				<input id="<?php echo $this->get_field_id( 'max_count' ); ?>" name="<?php echo $this->get_field_name( 'max_count' ); ?>" type="number" value="<?php echo $max_count ?>" size="3" />
+			</label> 
+		</p>
+		<p>
+			<input id="<?php echo $this->get_field_id( 'display_date' ); ?>" name="<?php echo $this->get_field_name( 'display_date' ); ?>" type="checkbox" <?php checked($instance['display_date'], 'on'); ?> />
+			<label for="<?php echo $this->get_field_id( 'display_date' ); ?>"><?php _e( 'Display category date?' ); ?></label>
+		</p>
+
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		
+		$instance = $old_instance;
+		$instance['max_count'] = ( ! empty( $new_instance['max_count'] ) ) ? strip_tags( $new_instance['max_count'] ) : 5;
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['display_date'] = $new_instance['display_date'] ;
+
+		return $instance;
+	}
+
+} // class Recent_Categories_Widget
